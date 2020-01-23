587a7a331efcf127e9b3aca0192678e29e7ea5b7
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-23 17:38:19 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -3,9 +3,8 @@ def using_while
   
     levitation_force = 6
     
-    
      while levitation_force < 10
-     
+     levitation_force += 1
      puts "Wingardium Leviosa"
   
     #levitation_force += 1
